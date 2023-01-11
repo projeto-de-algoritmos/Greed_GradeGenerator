4c1c81bd5cae6a62cd54f1d68fbf7255b267edbf
Victor Buendia <victorbuendia03@gmail.com>
2023-01-11 11:47:49 -0300

    Criação da tabela da grade e página principal

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Grade Generator!
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 class="text-center padding">Grade Generator</h1>
+
+      <p class="margin text-center">Instruções: pra você usar assim assim assado você vai fazer isso aqui e tal.</p>
+
+      <div class ="margin">
+        <table class="table table-striped table-hover">
+            <thead>
+              <tr>
+                <th scope="col">HORÁRIO</th>
+                <th scope="col">SEGUNDA</th>
+                <th scope="col">TERÇA</th>
+                <th scope="col">QUARTA</th>
+                <th scope="col">QUINTA</th>
+                <th scope="col">SEXTA</th>
+                <th scope="col">SÁBADO</th>
+                <th scope="col">DOMINGO</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">08:00</th>
+                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td><button type="button" class="btn">BOTÃO</button></td>                
+                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td><button type="button" class="btn">BOTÃO</button></td>
+              </tr>
+              <tr>
+                <th scope="row">09:00</th>
+                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td><button type="button" class="btn">BOTÃO</button></td>                
+                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td><button type="button" class="btn">BOTÃO</button></td>
+              </tr>
+              <tr>
+                <th scope="row">10:00</th>
+                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td><button type="button" class="btn">BOTÃO</button></td>                
+                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td><button type="button" class="btn">BOTÃO</button></td>
+              </tr>
+            </tbody>
+          </table>
+      </div>
     </div>
   );
 }
