4c3f060c1ae8388bf285682cc2a13fc7a5689c2c
Victor Buendia <victorbuendia03@gmail.com>
2023-01-11 14:41:56 -0300

    Melhora homepage

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,64 @@
 import './App.css';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
 
 function App() {
+
+  function FormFloatingBasicExample() {
+    return (
+      <>
+        <FloatingLabel
+          controlId="floatingTextarea"
+          className="mb-3"
+        >
+          <Form.Control as="input" placeholder="Nome da disciplina" />
+          <Form.Control as="input" placeholder="Sigla da disciplina" />
+        </FloatingLabel>
+      </>
+    );
+  }
+
+  function AddSubject() {
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+    return (
+      <>
+        <button type="button" onClick={handleShow} class="btn btn-danger btn-margin">ADICIONAR DISCIPLINA</button>
+  
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>ADICIONAR DISCIPLINA</Modal.Title>
+          </Modal.Header>
+          <Modal.Body><FormFloatingBasicExample/></Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              CANCELAR
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              INSERIR
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+
   return (
     <div className="App">
-      <h1 class="text-center padding">Grade Generator</h1>
+      <img alt="logo" class="center-img" width="1500px" src={require('./img/logo.png')} />
 
-      <p class="margin text-center">Instruções: pra você usar assim assim assado você vai fazer isso aqui e tal.</p>
+      <p class="margin text-center"><strong>INSTRUÇÕES:</strong> Para você usar a aplicação, basta clicar no botão abaixo para inserir uma disciplina que quer cursar no próximo semestre e considerar no gerador de grades. Depois disso, preencha as informações requisitadas. Quando inserir todas as disciplinas que quer ver nas grades geradas, clique no botão para gerar a grade.</p>
+      
+      <div class="center">
+        <AddSubject/>
+        <button type="button" class="btn btn-success btn-margin">GERAR GRADES</button>
+      </div>
 
       <div class ="margin">
         <table class="table table-striped table-hover">
@@ -24,37 +77,38 @@ function App() {
             <tbody>
               <tr>
                 <th scope="row">08:00</th>
-                <td><button type="button" class="btn">BOTÃO</button></td>
-                <td><button type="button" class="btn">BOTÃO</button></td>                
-                <td><button type="button" class="btn">BOTÃO</button></td>
-                <td><button type="button" class="btn">BOTÃO</button></td>
-                <td><button type="button" class="btn">BOTÃO</button></td>
-                <td><button type="button" class="btn">BOTÃO</button></td>
-                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td>VAZIO</td>
+                <td>VAZIO</td>                
+                <td>VAZIO</td>
+                <td>VAZIO</td>
+                <td>VAZIO</td>
+                <td>VAZIO</td>
+                <td>VAZIO</td>
               </tr>
               <tr>
                 <th scope="row">09:00</th>
-                <td><button type="button" class="btn">BOTÃO</button></td>
-                <td><button type="button" class="btn">BOTÃO</button></td>                
-                <td><button type="button" class="btn">BOTÃO</button></td>
-                <td><button type="button" class="btn">BOTÃO</button></td>
-                <td><button type="button" class="btn">BOTÃO</button></td>
-                <td><button type="button" class="btn">BOTÃO</button></td>
-                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td>VAZIO</td>
+                <td>VAZIO</td>                
+                <td>VAZIO</td>
+                <td>VAZIO</td>
+                <td>VAZIO</td>
+                <td>VAZIO</td>
+                <td>VAZIO</td>
               </tr>
               <tr>
                 <th scope="row">10:00</th>
-                <td><button type="button" class="btn">BOTÃO</button></td>
-                <td><button type="button" class="btn">BOTÃO</button></td>                
-                <td><button type="button" class="btn">BOTÃO</button></td>
-                <td><button type="button" class="btn">BOTÃO</button></td>
-                <td><button type="button" class="btn">BOTÃO</button></td>
-                <td><button type="button" class="btn">BOTÃO</button></td>
-                <td><button type="button" class="btn">BOTÃO</button></td>
+                <td>VAZIO</td>
+                <td>VAZIO</td>                
+                <td>VAZIO</td>
+                <td>VAZIO</td>
+                <td>VAZIO</td>
+                <td>VAZIO</td>
+                <td>VAZIO</td>
               </tr>
             </tbody>
           </table>
       </div>
+
     </div>
   );
 }
