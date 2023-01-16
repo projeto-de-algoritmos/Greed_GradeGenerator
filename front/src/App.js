24bb6e8f6c461d17c11c24d681801031f39cadbe
Jv Correia <correia.jv16@gmail.com>
2023-01-16 11:50:40 -0300

    Estilização do card

--- front/src/App.js
+++ front/src/App.js
@@ -9,41 +9,50 @@ function App() {
   const [home, setHome] = useState(true);
   const [interval, setInterval] = useState(false);
   const [minimize, setMinimize] = useState(false);
-  
+  const [variant, setVariant] = useState("light");
 
   const [intervalArray, setIntervalArray] = useState([]);
   const [minimizeArray, setMinimizeArray] = useState([]);
 
   function padWithLeadingZeros(num, totalLength) {
-    return String(num).padStart(totalLength, '0');
+    return String(num).padStart(totalLength, "0");
   }
 
   const Emoji = React.memo(({ className, label, symbol }) =>
-    symbol === "" ? <span></span> :
+    symbol === "" ? (
+      <span></span>
+    ) : (
       <span className={className} role="img" aria-label={label}>
         {String.fromCodePoint(symbol)}
-      </span>)
+      </span>
+    )
+  );
+
+  const changeColor = (color) => {
+    setVariant(color);
+    console.log(variant);
+  };
 
   const handleArrayChange = (data) => {
-    setArrayTasks(arrayTasks => [...arrayTasks, data]);
-  }
+    setArrayTasks((arrayTasks) => [...arrayTasks, data]);
+  };
 
   const handleIntervalArray = () => {
     setIntervalArray(intervalScheduling(arrayTasks));
-    setHome(false)
+    setHome(false);
     setInterval(true);
-    console.log(intervalArray)
-  }
+    console.log(intervalArray);
+  };
 
   const handleMinimize = () => {
     setMinimizeArray(minimizeLateness(arrayTasks));
-    setHome(false)
-    setMinimize(true)
-    console.log(minimizeArray)
-  }
+    setHome(false);
+    setMinimize(true);
+    console.log(minimizeArray);
+  };
 
   const handleRemoveItem = (obj) => {
-    setArrayTasks(arrayTasks.filter(item => item.name !== obj.name))
+    setArrayTasks(arrayTasks.filter((item) => item.name !== obj.name));
   };
 
   return (
@@ -57,90 +66,166 @@ function App() {
 
       <p class="margin text-center">
         <strong>INSTRUÇÕES:</strong> Para você usar a aplicação, basta clicar no
-        botão abaixo para inserir uma tarefa que precisa ser feita nos próximos dias. Depois disso, preencha as
-        informações requisitadas. Quando inserir todas as tarefas que quer
-        fazer, clique no botão para gerar a ordenação das atividades com o menor atraso.
+        botão abaixo para inserir uma tarefa que precisa ser feita nos próximos
+        dias. Depois disso, preencha as informações requisitadas. Quando inserir
+        todas as tarefas que quer fazer, clique no botão para gerar a ordenação
+        das atividades com o menor atraso.
       </p>
 
-      {home ? <><div class="center">
-        <AddModal handleArrayChange={handleArrayChange} />
-        <button type="button" class="btn btn-success btn-margin" onClick={() => handleMinimize()}>
-          MINIMIZE LATENESS
-        </button>
-        <button type="button" class="btn btn-info btn-margin" onClick={() => handleIntervalArray()}>
-          INTERVAL SCHEDULING
-        </button>
-      </div>
+      {home ? (
+        <>
+          <div class="center">
+            <AddModal handleArrayChange={handleArrayChange} />
+            <button
+              type="button"
+              class="btn btn-success btn-margin"
+              onClick={() => handleMinimize()}
+            >
+              MINIMIZE LATENESS
+            </button>
+            <button
+              type="button"
+              class="btn btn-info btn-margin"
+              onClick={() => handleIntervalArray()}
+            >
+              INTERVAL SCHEDULING
+            </button>
+          </div>
 
-        <div class="margin">
-          <table class="table table-striped table-hover">
-            <thead>
-              <tr class="margin text-center">
-                <th scope="col">NOME DA ATIVIDADE</th>
-                <th scope="col">DEADLINE</th>
-                <th scope="col">DURAÇÃO</th>
-                <th scope="col"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {arrayTasks.map((obj) => {
+          <div class="margin">
+            <table class="table table-striped table-hover">
+              <thead>
+                <tr class="margin text-center">
+                  <th scope="col">NOME DA ATIVIDADE</th>
+                  <th scope="col">DEADLINE</th>
+                  <th scope="col">DURAÇÃO</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {arrayTasks.map((obj) => {
+                  return (
+                    <tr class="margin text-center">
+                      <th scope="row">{obj.name}</th>
+                      <td>
+                        {obj.deadline.getDate() +
+                          "/" +
+                          padWithLeadingZeros(obj.deadline.getMonth() + 1, 2) +
+                          "/" +
+                          obj.deadline.getFullYear()}
+                      </td>
+                      <td>{obj.duration + " dias"}</td>
+                      <td className="center">
+                        <button
+                          type="button"
+                          class="btn"
+                          onClick={() => {
+                            handleRemoveItem(obj);
+                          }}
+                        >
+                          <Emoji className="hover" symbol="0x274C" />
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </>
+      ) : (
+        <div></div>
+      )}
+
+      {interval ? (
+        <>
+          <div class="center">
+            <button
+              type="button"
+              class="btn btn-warning btn-margin"
+              onClick={() => {
+                setHome(true);
+                setInterval(false);
+                setVariant("light");
+              }}
+            >
+              Voltar para página inicial
+            </button>
+          </div>
+          <div class="center">
+            <div class="w-50  ">
+              {intervalArray.map((obj) => {
                 return (
-                  <tr class="margin text-center">
-                    <th scope="row">{obj.name}</th>
-                    <td>{obj.deadline.getDate() + '/' + padWithLeadingZeros((obj.deadline.getMonth() + 1), 2) + '/' + obj.deadline.getFullYear()}</td>
-                    <td>{obj.duration + ' dias'}</td>
-                    <td className="center">
-                      <button type="button" class="btn" onClick={() => { handleRemoveItem(obj) }}>
-                        <Emoji className="hover" symbol="0x274C" />
-                      </button>
-                    </td>
-                  </tr>
+                  <div class="padding">
+                    <CardActivity
+                      variant={variant}
+                      header={obj.name}
+                      deadline={
+                        obj.deadline.getDate() +
+                        "/" +
+                        padWithLeadingZeros(obj.deadline.getMonth() + 1, 2) +
+                        "/" +
+                        obj.deadline.getFullYear()
+                      }
+                      description={obj.duration}
+                      changeColor={changeColor}
+                    >
+                      {" "}
+                    </CardActivity>
+                  </div>
                 );
               })}
-            </tbody>
-          </table>
-        </div></> : <div></div>}
+            </div>
+          </div>
+        </>
+      ) : (
+        <div> </div>
+      )}
 
-        {interval? 
-        <> 
-          <div class="center"> 
-              <button type="button" class="btn btn-warning btn-margin"  onClick={() => {setHome(true); setInterval(false)}}>
+      {minimize ? (
+        <>
+          <div class="center">
+            <button
+              type="button"
+              class="btn btn-warning btn-margin"
+              onClick={() => {
+                setHome(true);
+                setMinimize(false);
+              }}
+            >
               Voltar para página inicial
             </button>
-          </div> 
-          <div class="center"> 
-          <div class="w-50  " >
-          {intervalArray.map((obj)=>{
-            return(
-              <div class="padding"> 
-                <CardActivity header= {obj.name} deadline = {obj.deadline.getDate() + '/' + padWithLeadingZeros((obj.deadline.getMonth() + 1), 2) + '/' + obj.deadline.getFullYear()} description = {obj.duration}> </CardActivity>
-              </div>
-            )
-          })}
           </div>
+          <div class="center">
+            <div class="w-50  ">
+              {minimizeArray.map((obj) => {
+                return (
+                  <div class="padding">
+                    <CardActivity
+                      variant={variant}
+                      header={obj.name}
+                      deadline={
+                        obj.deadline.getDate() +
+                        "/" +
+                        padWithLeadingZeros(obj.deadline.getMonth() + 1, 2) +
+                        "/" +
+                        obj.deadline.getFullYear()
+                      }
+                      description={obj.duration}
+                      lateness={obj.lateness}
+                      changeColor={changeColor}
+                    >
+                      {" "}
+                    </CardActivity>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-        </> : <div> </div>}
-
-        {minimize? 
-        <> 
-          <div class="center"> 
-              <button type="button" class="btn btn-warning btn-margin"  onClick={() => {setHome(true); setMinimize(false)}}>
-              Voltar para página inicial
-            </button>
-          </div> 
-          <div class="center"> 
-          <div class="w-50  " >
-          {minimizeArray.map((obj)=>{
-            return(
-              <div class="padding"> 
-                <CardActivity header= {obj.name} deadline = {obj.deadline.getDate() + '/' + padWithLeadingZeros((obj.deadline.getMonth() + 1), 2) + '/' + obj.deadline.getFullYear()} description = {obj.duration} lateness = {obj.lateness}> </CardActivity>
-              </div>
-            )
-          })}
-          </div>
-          </div>
-        </> : <div> </div>}
-
+        </>
+      ) : (
+        <div> </div>
+      )}
     </div>
   );
 }
--- front/src/components/card.js
+++ front/src/components/card.js
@@ -1,32 +1,60 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { Button, Image } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import check from "../img/check.png";
+import x from "../img/x.png";
 
 const CardActivity = (props) => {
-    return (
-        <Card>
-            <Card.Header as="h5">{props.header}</Card.Header>
-            <Card.Body>
-                <Card.Title> Deadline: {props.deadline}</Card.Title>
-                <div class="container mb-3" >
-                    <div class="row">
-                        <div class="col">
-                            <Card.Text>
-                                <b> Dias de duração:</b> {props.description}
-                            </Card.Text>
-                        </div>
-                        {props.lateness && <div class="col">
-                            <Card.Text>
-                                <b> Tempo de atraso: </b>{props.lateness}
-                            </Card.Text>
-                        </div>}
-                    </div>
-                </div>
-                <div class="center">
-                    <Button variant="primary"> Feito! </Button>
-                </div>
-            </Card.Body>
-        </Card>
-    );
-}
+  return (
+    <Card bg={props.variant}>
+      <Card.Header as="h5">{props.header}</Card.Header>
+      <Card.Body>
+        <div class="container mb-3 center">
+          <div class="row w-100 center">
+            <div class="col">
+              {" "}
+              <Card.Text>
+                {" "}
+                <b> Deadline: </b> {props.deadline}
+              </Card.Text>
+            </div>
+
+            <div class="col center">
+              <Card.Text>
+                <b> Dias de duração:</b> {props.description}
+              </Card.Text>
+            </div>
+            {props.lateness && props.lateness !== 0 ? (
+              <div class="col">
+                <Card.Text>
+                  <b> Tempo de atraso: </b>
+                  {props.lateness}
+                </Card.Text>
+              </div>
+            ) : (
+              <> </>
+            )}
+          </div>
+        </div>
+        <div class="center">
+          {props.variant === "light" ? (
+            <Button
+              variant="primary"
+              onClick={() => props.changeColor("success")}
+            >
+              <Image padding="0" src={check} width="250px" alt="check"></Image>
+            </Button>
+          ) : (
+            <Button
+              variant="primary"
+              onClick={() => props.changeColor("light")}
+            >
+              <Image padding="0" src={x} width="50px" alt="check"></Image>
+            </Button>
+          )}
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default CardActivity;
